9b40bf116e14bcead06beddcac07952634572e55
홍정훈 <2jh627@naver.com>
2025-09-30 16:49:03 +0900

    FeignException ErrorDecode (11. Microservice communication)

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -89,6 +90,13 @@ public class UserServiceImpl implements UserService {
 //                        new ParameterizedTypeReference<List<ResponseOrder>>() {});
 //        List<ResponseOrder> orderList = orderListResponse.getBody();
 
+//        List<ResponseOrder> orderList = null;
+//        try {
+//            orderServiceClient.getOrders(userId);
+//        }
+//        catch (FeignException ex) {
+//            log.error(ex.getMessage());
+//        }
         List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
         userDto.setOrders(orderList);
 
